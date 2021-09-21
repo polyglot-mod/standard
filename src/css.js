7b3b7f1aabcf7669d963119c3dfe21de1bf5ab8d
Oj <oj@oojmed.com>
2021-09-21 18:13:42 +0100

    [CSS] Add removal

--- src/css.js
+++ src/css.js
@@ -1,7 +1,17 @@
+const els = [];
+
 export const add = (css) => {
   const el = document.createElement('style');
 
   el.appendChild(document.createTextNode(css));
 
   document.body.appendChild(el);
+
+  els.push(el);
+};
+
+export const remove = () => {
+  for (const el of els) {
+    el.remove():
+  }
 };
