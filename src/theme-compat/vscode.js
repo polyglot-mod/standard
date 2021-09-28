106db4f9e5d726208a29b6948c8b361bcd6dbfea
CanadaHonk <19228318+CanadaHonk@users.noreply.github.com>
2021-09-28 13:03:34 +0100

    [Theme Compat > VSCode] Initial Add

---
+++ src/theme-compat/vscode.js
@@ -0,0 +1,33 @@
+let CSS;
+
+export const add = async (url) => {
+  CSS = await import(`https://polyglot-mod.github.io/standard/src/css.js?_${Date.now()}`);
+
+  const theme = await (await fetch(url)).json();
+
+  CSS.add(`.theme-dark, .theme-light {
+    --background-primary: ${theme.colors['editor.background']}; /* Main editor background */
+    --background-secondary: ${theme.colors['sideBar.background']}; /* Sidebar (left or right with file explorer, etc.) */
+    --background-secondary-alt: ${theme.colors['tab.hoverBackground']};
+    --background-tertiary: ${theme.colors['tab.border']};
+  
+    --channeltextarea-background: var(--background-tertiary);
+  
+    --background-accent: ${theme.colors['input.background']}; /* Background for inputs */
+    --background-floating: ${theme.colors['button.secondaryBackground']}; /* Background for buttons */
+  
+    --brand-experiment: ${theme.colors['textLink.foreground']}; /* Background for links */
+  
+    --text-link: var(--brand-experiment);
+    --text-normal: ${theme.colors['editor.foreground']}; /* Foreground for main editor */
+    --text-muted: ${theme.colors['editorActiveLineNumber.foreground']};
+    --interactive-normal: ${theme.colors['descriptionForeground']}; /* Foreground for descriptions */
+    --interactive-hover: var(--text-normal);
+    --interactive-active: ${theme.colors['settings.headerForeground']}; /* Foreground for headers */
+    --interactive-muted: var(--text-muted);
+  }`);
+};
+
+export const remove = () => {
+  CSS.remove();
+};
