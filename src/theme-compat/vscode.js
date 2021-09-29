958c24d601870e30a4c3e42c9e948a15d929f548
Oj <oj@oojmed.com>
2021-09-29 21:12:46 +0100

    [Theme Compat > VSCode] Switch background accent and background secondary alt

--- src/theme-compat/vscode.js
+++ src/theme-compat/vscode.js
@@ -19,12 +19,12 @@ export const add = async (theme) => {
   CSS.add(`.theme-dark, .theme-light {
     --background-primary: ${theme.colors['editor.background']}; /* Main editor background */
     --background-secondary: ${theme.colors['sideBar.background']}; /* Sidebar (left or right with file explorer, etc.) */
-    --background-secondary-alt: ${theme.colors['editorIndentGuide.background']};
+    --background-secondary-alt: ${theme.colors['input.background']};
     --background-tertiary: ${theme.colors['activityBar.background']};
   
     --channeltextarea-background: var(--background-secondary-alt);
   
-    --background-accent: ${theme.colors['input.background']}; /* Background for inputs */
+    --background-accent: ${theme.colors['editorIndentGuide.background']}; /* Background for inputs */
     --background-floating: ${theme.colors['button.secondaryBackground']}; /* Background for buttons */
   
     --brand-experiment: ${theme.colors['textLink.foreground']}; /* Background for links */
