a693a29bcef677ebeb5a8a8a257707676f50c618
Oj <oj@oojmed.com>
2021-10-13 10:45:08 +0100

    [CSS] Debugging

--- css.js
+++ css.js
@@ -45,6 +45,10 @@ export const getAllRules = function () { // Gets all CSS stylesheet rules
 };
 
 export const remap = function (vars) {
+  // if (document.getElementById('polymod_css_cache')) document.getElementById('polymod_css_cache').remove(); // Remove CSS cache if exists as it breaks ?
+
+  console.log('css remap', vars, getAllRules());
+
   let finalCss = '';
 
   const extraVars = [ '--background-accent', '--background-floating' ];
